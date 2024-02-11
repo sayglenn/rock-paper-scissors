15a9284c86edf4aa3415b5c3c82f627a4d56a1e3
Say Glenn <glennsay1234@gmail.com>
2024-02-11 13:44:31 +0800

    Added a display for choices

--- script.js
+++ script.js
@@ -2,38 +2,39 @@
 function getComputerChoice() {
     let randomInt = Math.floor(Math.random() * 3);
     return randomInt == 0
-        ? "ROCK"
+        ? "Rock"
         : randomInt == 1
-        ? "PAPER"
-        : "SCISSORS";
+        ? "Paper"
+        : "Scissors";
 }
 
 function generateResult(playerSelection, computerSelection) {
-    if (playerSelection == "ROCK") {
-        return computerSelection == "ROCK"
+    if (playerSelection == "Rock") {
+        return computerSelection == "Rock"
             ? "It's a draw! Rock ties against Rock."
-            : computerSelection == "PAPER"
+            : computerSelection == "Paper"
             ? "You Lose! Rock loses to Paper."
             : "You Win! Rock beats Scissors!"
-    } else if (playerSelection == "PAPER") {
-        return computerSelection == "ROCK"
+    } else if (playerSelection == "Paper") {
+        return computerSelection == "Rock"
             ? "You Win! Paper beats Rock!"
-            : computerSelection == "PAPER"
+            : computerSelection == "Paper"
             ? "It's a draw! Paper ties against Paper."
             : "You Lose! Paper loses to Scissors."
     } else {
-        return computerSelection == "ROCK"
+        return computerSelection == "Rock"
             ? "You Lose! Scissors loses to Rock."
-            : computerSelection == "PAPER"
+            : computerSelection == "Paper"
             ? "You Win! Scissors beats Paper!"
-            : "It's a draw! Paper ties against Paper."
+            : "It's a draw! Scissors ties against Scissors."
     }
 }
 
 function resultChecker(string) {
     if (string.slice(0, 5) == "You W") {
         playerScore += 1;
-        playerText.textContent = "Player: " + playerScore;
+        playerText.textContent = "Player: " + playerScore + " (" + playerChoice + ")";
+        computerText.textContent = "Computer: " + computerScore + " (" + computerChoice + ")";
         if (playerScore == 5) {
             resultText.textContent = "You won this round! The final score is " + 
                 playerScore + " to " + computerScore + "!";
@@ -45,6 +46,8 @@ function resultChecker(string) {
     } else if (string.slice(0, 5) == "You L") {
         computerScore += 1;
         computerText.textContent = "Computer: " + computerScore;
+        playerText.textContent = "Player: " + playerScore + " (" + playerChoice + ")";
+        computerText.textContent = "Computer: " + computerScore + " (" + computerChoice + ")";
         if (computerScore == 5) {
             resultText.textContent = "You lost this round! The final score is " + 
                 playerScore + " to " + computerScore + "!";
@@ -54,12 +57,15 @@ function resultChecker(string) {
             computerText.textContent = "Computer: " + computerScore;
         }
     } else {
-
+        playerText.textContent = "Player: " + playerScore + " (" + playerChoice + ")";
+        computerText.textContent = "Computer: " + computerScore + " (" + computerChoice + ")";
     }
 }
 
 let playerScore = 0;
 let computerScore = 0;
+let playerChoice = "";
+let computerChoice = "";
 
 const rockChoice = document.querySelector("#rock-button");
 const paperChoice = document.querySelector("#paper-button");
@@ -70,19 +76,25 @@ const computerText = document.querySelector(".result-computer");
 const resultText = document.querySelector(".result-text");
 
 rockChoice.onclick = () => {
-    const result = generateResult("ROCK", getComputerChoice());
+    playerChoice = "Rock"
+    computerChoice = getComputerChoice();
+    const result = generateResult(playerChoice, computerChoice);
     resultText.textContent = result;
     resultChecker(result);
 }
 
 paperChoice.onclick = () => {
-    const result = generateResult("PAPER", getComputerChoice());
+    playerChoice = "Paper"
+    computerChoice = getComputerChoice();
+    const result = generateResult(playerChoice, getComputerChoice());
     resultText.textContent = result;
     resultChecker(result);
 }
 
 scissorsChoice.onclick = () => {
-    const result = generateResult("SCISSORS", getComputerChoice());
+    playerChoice = "Scissors";
+    computerChoice = getComputerChoice();
+    const result = generateResult(playerChoice, getComputerChoice());
     resultText.textContent = result;
     resultChecker(result);
 }
